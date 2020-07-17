df7e55897beaeabccae6e4250955e88535e2ed15
Jorge Venegas <brangelina01@gmail.com>
2020-07-17 05:40:50 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -3,13 +3,11 @@
 #   "Sarah" => 6, 
 #   "Amanda" => 27
 # }
-
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end	
-
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     if age < 12
@@ -19,4 +17,3 @@ def age_appropriate_birthday(birthday_kids)
     end
   end
 end
-
